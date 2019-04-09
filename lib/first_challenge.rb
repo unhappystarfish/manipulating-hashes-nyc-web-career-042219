9796eb06a57d27dfec6498c0c23076edabb7bce3
Jake L <jakelovitz@gmail.com>
2019-04-09 16:13:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,9 +16,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        value.each do |flavor|
           flavor.delete_if {|x| x = "strawberry" }
-        end
       end
     end
   end
